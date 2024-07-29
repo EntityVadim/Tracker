bcca8871bb0ab09897aa05c73c043a98adf4eaab
EntityVadim <21930@mail.ru>
2024-07-29 16:05:38 +0300

    - Добавил иконки для верстки кодом; - Сверстал главный экран.

--- Tracker/TrackersViewController.swift
+++ Tracker/TrackersViewController.swift
@@ -10,53 +10,132 @@ import UIKit
 
 final class TrackersViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        // Настройка заголовка
-        let titleLabel = UILabel()
-        titleLabel.text = "Трекеры"
-        titleLabel.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(titleLabel)
-        
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 88),
-            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
-        ])
-        
-        // Настройка поисковой строки
+    // MARK: - Private Properties
+    
+    private let addButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "Plus"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let dateContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .ypLightGray
+        view.layer.cornerRadius = 4
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Трекеры"
+        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Поиск"
         searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    private let errorImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "Error"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let trackingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Что будем отслеживать?"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(addButton)
+        NSLayoutConstraint.activate([
+            addButton.widthAnchor.constraint(equalToConstant: 42),
+            addButton.heightAnchor.constraint(equalToConstant: 42),
+            addButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 45),
+            addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 6)
+        ])
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yy"
+        let currentDate = dateFormatter.string(from: Date())
+        dateLabel.text = currentDate
+        view.addSubview(dateContainer)
+        dateContainer.addSubview(dateLabel)
+        NSLayoutConstraint.activate([
+            dateContainer.widthAnchor.constraint(equalToConstant: 77),
+            dateContainer.heightAnchor.constraint(equalToConstant: 34),
+            dateContainer.topAnchor.constraint(equalTo: view.topAnchor, constant: 49),
+            dateContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            dateLabel.widthAnchor.constraint(equalToConstant: 66),
+            dateLabel.heightAnchor.constraint(equalToConstant: 22),
+            dateLabel.centerYAnchor.constraint(equalTo: dateContainer.centerYAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: dateContainer.trailingAnchor, constant: -5)
+        ])
+        
+        view.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: addButton.bottomAnchor, constant: 1),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
         
         view.addSubview(searchBar)
-        
         NSLayoutConstraint.activate([
-            searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            searchBar.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 7),
             searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             searchBar.heightAnchor.constraint(equalToConstant: 36)
         ])
         
-        let addButton = UIButton(type: .custom)
-        addButton.setImage(UIImage(named: "Plus"), for: .normal)
-        addButton.translatesAutoresizingMaskIntoConstraints = false
-        addButton.addTarget(self, action: #selector(addTracker), for: .touchUpInside)
-        
-        view.addSubview(addButton)
-        
+        view.addSubview(errorImageView)
         NSLayoutConstraint.activate([
-            addButton.widthAnchor.constraint(equalToConstant: 42),
-            addButton.heightAnchor.constraint(equalToConstant: 42),
-            addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 45),
-            addButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 6)
+            errorImageView.widthAnchor.constraint(equalToConstant: 80),
+            errorImageView.heightAnchor.constraint(equalToConstant: 80),
+            errorImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 402),
+            errorImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 147)
+        ])
+        
+        view.addSubview(trackingLabel)
+        NSLayoutConstraint.activate([
+            trackingLabel.topAnchor.constraint(equalTo: errorImageView.bottomAnchor, constant: 8),
+            trackingLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            trackingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            trackingLabel.heightAnchor.constraint(equalToConstant: 18)
         ])
     }
     
-    @objc func addTracker() {
+    // MARK: - Actions
+    
+    @objc private func addTracker() {
         // Действие для добавления трекера
     }
 }
