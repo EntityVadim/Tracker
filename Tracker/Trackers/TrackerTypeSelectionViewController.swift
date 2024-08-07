490a41ae3519f2e5031fd6b3d82005ab08875749
EntityVadim <21930@mail.ru>
2024-08-07 14:56:18 +0300

    - Сделал задание: 1. Реализованы экраны создания трекера:  - при нажатии на кнопку + на главном экране показывается выбор типа трекера («Привычка» или «Нерегулярное событие»).

---
+++ Tracker/Trackers/TrackerTypeSelectionViewController.swift
@@ -0,0 +1,108 @@
+//
+//  TrackerTypeSelectionViewController.swift
+//  Tracker
+//
+//  Created by Вадим on 07.08.2024.
+//
+
+import UIKit
+
+protocol TrackerTypeSelectionDelegate: AnyObject {
+    func didSelectTrackerType(_ type: TrackerType)
+}
+
+enum TrackerType {
+    case habit
+    case irregularEvent
+}
+
+final class TrackerTypeSelectionViewController: UIViewController {
+    
+    weak var delegate: TrackerTypeSelectionDelegate?
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Создание трекера"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        return label
+    }()
+    
+    private lazy var habitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Привычка", for: .normal)
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.addTarget(
+            self,
+            action: #selector(habitButtonTapped),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var irregularEventButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Нерегулярное событие", for: .normal)
+        button.layer.cornerRadius = 16
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        button.addTarget(
+            self,
+            action: #selector(irregularEventButtonTapped),
+            for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        setupAppearance()
+    }
+    
+    private func setupUI() {
+        [titleLabel,
+         habitButton,
+         irregularEventButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
+            
+            habitButton.widthAnchor.constraint(equalToConstant: 335),
+            habitButton.heightAnchor.constraint(equalToConstant: 60),
+            habitButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            habitButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
+            
+            irregularEventButton.widthAnchor.constraint(equalToConstant: 335),
+            irregularEventButton.heightAnchor.constraint(equalToConstant: 60),
+            irregularEventButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            irregularEventButton.topAnchor.constraint(equalTo: habitButton.bottomAnchor, constant: 16)
+        ])
+    }
+    
+    private func setupAppearance() {
+        let isDarkMode = traitCollection.userInterfaceStyle == .dark
+        view.backgroundColor = isDarkMode ? .ypBlack : .ypWhite
+        titleLabel.textColor = isDarkMode ? .ypWhite : .ypBlack
+        habitButton.backgroundColor = isDarkMode ? .ypWhite : .ypBlack
+        habitButton.setTitleColor(isDarkMode ? .ypBlack : .ypWhite, for: .normal)
+        irregularEventButton.backgroundColor = isDarkMode ? .ypWhite : .ypBlack
+        irregularEventButton.setTitleColor(isDarkMode ? .ypBlack : .ypWhite, for: .normal)
+    }
+    
+    @objc private func habitButtonTapped() {
+        delegate?.didSelectTrackerType(.habit)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func irregularEventButtonTapped() {
+        delegate?.didSelectTrackerType(.irregularEvent)
+        dismiss(animated: true, completion: nil)
+    }
+}
