c45357dba7540a85622653490680911532cfb5c6
EntityVadim <21930@mail.ru>
2024-08-08 19:36:18 +0300

    - Добавил таблицу в TrackerCategoryViewController.

--- Tracker/Trackers/TrackerCategoryViewController.swift
+++ Tracker/Trackers/TrackerCategoryViewController.swift
@@ -33,8 +33,21 @@ final class TrackerCategoryViewController: UIViewController {
         return button
     }()
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryCell")
+        tableView.separatorStyle = .singleLine
+        tableView.separatorColor = UIColor.ypGrey
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        tableView.rowHeight = 60 // Не понимаю, почему не работает
+        return tableView
+    }()
+    
     private var categories: [String] = [] {
         didSet {
+            tableView.reloadData()
         }
     }
     
@@ -50,7 +63,8 @@ final class TrackerCategoryViewController: UIViewController {
     
     private func setupUI() {
         [titleLabel,
-         addCategoryButton].forEach {
+         addCategoryButton,
+         tableView].forEach {
             view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -62,7 +76,12 @@ final class TrackerCategoryViewController: UIViewController {
             addCategoryButton.heightAnchor.constraint(equalToConstant: 60),
             addCategoryButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             addCategoryButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            addCategoryButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+            addCategoryButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
+            
+            tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 24),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: addCategoryButton.topAnchor, constant: -20)
         ])
     }
     
@@ -72,7 +91,63 @@ final class TrackerCategoryViewController: UIViewController {
         let newCategoryVC = TrackerNewCategoryViewController()
         newCategoryVC.onCategorySave = { [weak self] categoryName in
             self?.categories.append(categoryName)
+            self?.tableView.reloadData()
         }
         present(newCategoryVC, animated: true, completion: nil)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension TrackerCategoryViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return categories.count
+        }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+            cell.textLabel?.text = categories[indexPath.row]
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            cell.contentView.backgroundColor = .ypLightGray
+            return cell
+        }
+}
+
+// MARK: - UITableViewDelegate
+
+extension TrackerCategoryViewController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        willDisplay cell: UITableViewCell,
+        forRowAt indexPath: IndexPath) {
+            if indexPath.row == 0 && categories.count == 1 {
+                cell.contentView.layer.cornerRadius = 16
+                cell.contentView.layer.maskedCorners = [
+                    .layerMinXMinYCorner,
+                    .layerMaxXMinYCorner,
+                    .layerMinXMaxYCorner,
+                    .layerMaxXMaxYCorner]
+            } else if indexPath.row == 0 {
+                cell.contentView.layer.cornerRadius = 16
+                cell.contentView.layer.maskedCorners = [
+                    .layerMinXMinYCorner,
+                    .layerMaxXMinYCorner]
+            } else if indexPath.row == categories.count - 1 {
+                cell.contentView.layer.cornerRadius = 16
+                cell.contentView.layer.maskedCorners = [
+                    .layerMinXMaxYCorner,
+                    .layerMaxXMaxYCorner]
+            } else {
+                cell.contentView.layer.cornerRadius = 0
+            }
+            cell.contentView.layer.masksToBounds = true
+        }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
