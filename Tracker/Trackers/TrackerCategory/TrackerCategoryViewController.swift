449df06a5b0c24b2355c54928c9962c1c3c7f246
EntityVadim <21930@mail.ru>
2024-08-15 14:54:06 +0300

    - Сделал более читаемым код.

--- Tracker/Trackers/TrackerCategory/TrackerCategoryViewController.swift
+++ Tracker/Trackers/TrackerCategory/TrackerCategoryViewController.swift
@@ -15,6 +15,19 @@ final class TrackerCategoryViewController: UIViewController {
     
     var categorySelectionHandler: ((String) -> Void)?
     
+    var categories: [String] = [] {
+        didSet {
+            saveCategories()
+        }
+    }
+    
+    var selectedCategory: String? {
+        didSet {
+            saveSelectedCategory()
+            tableView.reloadData()
+        }
+    }
+    
     // MARK: - Private Properties
     
     private let titleLabel: UILabel = {
@@ -51,17 +64,6 @@ final class TrackerCategoryViewController: UIViewController {
         tableView.rowHeight = 75
         return tableView
     }()
-    
-    private var categories: [String] = [] {
-        didSet { saveCategories() }
-    }
-    
-    private var selectedCategory: String? {
-        didSet {
-            saveSelectedCategory()
-            tableView.reloadData()
-        }
-    }
     
     // MARK: - Lifecycle
     
@@ -133,74 +135,5 @@ final class TrackerCategoryViewController: UIViewController {
         }
         categorySelectionHandler?(selectedCategory)
         dismiss(animated: true, completion: nil)
-    }
-}
-
-// MARK: - UITableViewDataSource
-
-extension TrackerCategoryViewController: UITableViewDataSource {
-    func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int) -> Int {
-            return categories.count
-        }
-    
-    func tableView(
-        _ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            guard let cell = tableView.dequeueReusableCell(
-                withIdentifier: "CustomCategoryCell",
-                for: indexPath) as? TrackerCategoryCell else {
-                return UITableViewCell()
-            }
-            let category = categories[indexPath.row]
-            let isSelected = category == selectedCategory
-            cell.configure(with: category, isSelected: isSelected)
-            cell.contentView.backgroundColor = .ypLightGray
-            return cell
-        }
-}
-
-// MARK: - UITableViewDelegate
-
-extension TrackerCategoryViewController: UITableViewDelegate {
-    func tableView(
-        _ tableView: UITableView,
-        willDisplay cell: UITableViewCell,
-        forRowAt indexPath: IndexPath) {
-            if indexPath.row == 0 && categories.count == 1 {
-                cell.contentView.layer.cornerRadius = 16
-                cell.contentView.layer.maskedCorners = [
-                    .layerMinXMinYCorner,
-                    .layerMaxXMinYCorner,
-                    .layerMinXMaxYCorner,
-                    .layerMaxXMaxYCorner]
-            } else if indexPath.row == 0 {
-                cell.contentView.layer.cornerRadius = 16
-                cell.contentView.layer.maskedCorners = [
-                    .layerMinXMinYCorner,
-                    .layerMaxXMinYCorner]
-            } else if indexPath.row == categories.count - 1 {
-                cell.contentView.layer.cornerRadius = 16
-                cell.contentView.layer.maskedCorners = [
-                    .layerMinXMaxYCorner,
-                    .layerMaxXMaxYCorner]
-            } else {
-                cell.contentView.layer.cornerRadius = 0
-            }
-            cell.contentView.layer.masksToBounds = true
-        }
-    
-    func tableView(
-        _ tableView: UITableView,
-        didSelectRowAt indexPath: IndexPath
-    ) {
-        let category = categories[indexPath.row]
-        if category == selectedCategory {
-            selectedCategory = nil
-        } else {
-            selectedCategory = category
-        }
-        tableView.reloadData()
     }
 }
---
+++ Tracker/Trackers/TrackerSchedule/ExtensionTrackerSchedule.swift
@@ -0,0 +1,55 @@
+//
+//  ExtensionTrackerSchedule.swift
+//  Tracker
+//
+//  Created by Вадим on 15.08.2024.
+//
+
+import UIKit
+
+// MARK: - UITableViewDelegate & UITableViewDataSource
+
+extension TrackerScheduleViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return WeekDay.allCases.count
+        }
+    
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let day = WeekDay.allCases[indexPath.row]
+            cell.textLabel?.text = day.rawValue
+            let switchView = UISwitch()
+            switchView.isOn = selectedDays.contains(day)
+            switchView.addTarget(self, action: #selector(switchChanged(sender:)), for: .valueChanged)
+            switchView.onTintColor = UIColor.ypBlue
+            cell.backgroundColor = UIColor.ypLightGray
+            cell.accessoryView = switchView
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+            return cell
+        }
+    
+    func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 75
+        }
+    
+    @objc func switchChanged(sender: UISwitch) {
+        guard let cell = sender.superview as? UITableViewCell,
+              let indexPath = tableView.indexPath(for: cell) else {
+            return
+        }
+        let day = WeekDay.allCases[indexPath.row]
+        if sender.isOn {
+            selectedDays.append(day)
+        } else {
+            if let index = selectedDays.firstIndex(of: day) {
+                selectedDays.remove(at: index)
+            }
+        }
+    }
+}
