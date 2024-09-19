8f053229f0c90451f8649a6a52dca736df363d22
EntityVadim <21930@mail.ru>
2024-09-19 19:06:54 +0300

    - Исправил баг.

--- Tracker/Trackers/TrackerCreation/TrackerCreationViewController.swift
+++ Tracker/Trackers/TrackerCreation/TrackerCreationViewController.swift
@@ -358,7 +358,17 @@ final class TrackerCreationViewController: UIViewController, UITextFieldDelegate
         selectedEmoji = tracker.emoji
         selectedColor = tracker.color
         selectedDays = tracker.schedule.compactMap { WeekDay(rawValue: $0) }
-        updateButtonStates()
+        if selectedDays.isEmpty {
+            trackerType = .irregularEvent
+            scheduleButton.isHidden = true
+            separatorView.isHidden = true
+            updateCategoriesButtonCorners(.allCorners, radius: 16)
+        } else {
+            trackerType = .habit
+            scheduleButton.isHidden = false
+            separatorView.isHidden = false
+            updateCategoriesButtonCorners([.topLeft, .topRight], radius: 16)
+        }
         updatePlainCategoriesButtonTitle(categoryTitle: category)
         updateScheduleButtonTitle()
         updateSaveButtonState()
@@ -477,17 +487,6 @@ final class TrackerCreationViewController: UIViewController, UITextFieldDelegate
             separatorView.isHidden = true
             updateCategoriesButtonCorners(.allCorners, radius: 16)
         } else {
-            updateCategoriesButtonCorners([.topLeft, .topRight], radius: 16)
-        }
-        if selectedDays.isEmpty {
-            trackerType = .irregularEvent
-            scheduleButton.isHidden = true
-            separatorView.isHidden = true
-            updateCategoriesButtonCorners(.allCorners, radius: 16)
-        } else {
-            trackerType = .habit
-            scheduleButton.isHidden = false
-            separatorView.isHidden = false
             updateCategoriesButtonCorners([.topLeft, .topRight], radius: 16)
         }
     }
