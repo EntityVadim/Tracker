629bfaa453264fffe50e36c6599ce8b0975c6d45
EntityVadim <21930@mail.ru>
2024-09-21 19:17:57 +0300

    - Добавил градиентную рамку и два лейбла внутрь.

--- Tracker/Statistics/StatisticsViewController.swift
+++ Tracker/Statistics/StatisticsViewController.swift
@@ -22,6 +22,58 @@ final class StatisticsViewController: UIViewController {
         return label
     }()
     
+    private lazy var gradientView: UIView = {
+        let view = UIView()
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.ypSelection3.cgColor,
+            UIColor.ypSelection9.cgColor,
+            UIColor.ypSelection1.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 90)
+        view.layer.addSublayer(gradientLayer)
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        let maskLayer = CAShapeLayer()
+        let outerPath = UIBezierPath(roundedRect: CGRect(
+            x: 0, y: 0,
+            width: self.view.bounds.width - 32,
+            height: 90), cornerRadius: 16)
+        let innerPath = UIBezierPath(roundedRect: CGRect(
+            x: 1, y: 1,
+            width: self.view.bounds.width - 34,
+            height: 88), cornerRadius: 15)
+        outerPath.append(innerPath)
+        outerPath.usesEvenOddFillRule = true
+        maskLayer.path = outerPath.cgPath
+        maskLayer.fillRule = .evenOdd
+        view.layer.mask = maskLayer
+        [firstLabel, secondLabel].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        return view
+    }()
+    
+    private lazy var firstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "50"
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .center
+        label.textColor = .ypBlack
+        return label
+    }()
+    
+    private lazy var secondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textAlignment = .center
+        label.textColor = .ypBlack
+        return label
+    }()
+    
     private lazy var errorImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "ErrorStat"))
         return imageView
@@ -49,7 +101,7 @@ final class StatisticsViewController: UIViewController {
     // MARK: - Setup UI
     
     private func setupUI() {
-        [titleLabel, errorImageView, placeholderLabel].forEach {
+        [titleLabel, gradientView, errorImageView, placeholderLabel].forEach {
             view.addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -59,6 +111,20 @@ final class StatisticsViewController: UIViewController {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 88),
             titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            gradientView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 77),
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            gradientView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            gradientView.heightAnchor.constraint(equalToConstant: 90),
+            
+            firstLabel.topAnchor.constraint(equalTo: gradientView.topAnchor, constant: 12),
+            firstLabel.leadingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 12),
+            firstLabel.trailingAnchor.constraint(equalTo: gradientView.trailingAnchor, constant: -12),
+            
+            secondLabel.topAnchor.constraint(equalTo: firstLabel.bottomAnchor),
+            secondLabel.leadingAnchor.constraint(equalTo: gradientView.leadingAnchor, constant: 12),
+            secondLabel.trailingAnchor.constraint(equalTo: gradientView.trailingAnchor, constant: -12),
+            secondLabel.bottomAnchor.constraint(equalTo: gradientView.bottomAnchor, constant: -12),
             
             errorImageView.widthAnchor.constraint(equalToConstant: 80),
             errorImageView.heightAnchor.constraint(equalToConstant: 80),
